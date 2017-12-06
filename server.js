e8015b6a9268323254c90484e97b354a7b117a92
kconnelly86 <kconnelly86@yahoo.com>
2017-12-06 09:02:30 -0600

    scrapes site gets title only

--- server.js
+++ server.js
@@ -9,13 +9,23 @@ var cheerio = require('cheerio');
 //require all models
 var db = require("./models");
 
+var path  = require('path')
+
 var PORT = 3000;
 //initialize express
 var app = express();
 //database
 var mongoose = require("mongoose");
 
+
+// Use morgan logger for logging requests
+//app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// this creates the DB have to have this.
 // Set mongoose to leverage built in JavaScript ES6 Promises
+
+
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/Article", {
@@ -31,16 +41,17 @@ app.get('/scrape', function (req, res) {
 		  //console.log(response);
 		 var $ = cheerio.load(body);
 		 // console.log(body);
+      
 		 $("h2.title a").each(function(i, element) {
 
-	 		console.log(element.children[0].data);
 	 		var myStuff = {};
-	 		myStuff.title = element.children[0].data;
-	 		console.log(myStuff);
+       myStuff.title = element.children[0].data;
+
+
 	 		// Create a new Article using the `result` object built from scraping
 	        db.Article
 	        .create(myStuff)
-	        
+
 	        .then(function(dbArticle) {
 	          // If we were able to successfully scrape and save an Article, send a message to the client
 	          res.send("Scrape Complete");
@@ -54,22 +65,44 @@ app.get('/scrape', function (req, res) {
 	});//end request
 });// end app .get
 
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
-// / Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article
-//     .find({})
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
 
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article
+    .find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+      
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+	console.log(req.params.id)
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article
+    .findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    // .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 
 
